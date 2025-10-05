7a545eefb8fc182f7d5e2a1f7407e3a2942ebe0b
Petr Bílek <p.bilek29@gmail.com>
2025-10-05 12:52:34 +0200

    [add] Shared component for section titles

---
+++ src/components/shared/sectionTitle/SectionTitle.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+interface SectionTitleProps {
+  preheadline: string;
+  headline: string;
+  variant?: 'light' | 'dark';
+}
+
+export default function SectionTitle({
+  preheadline,
+  headline,
+  variant = 'dark',
+}: SectionTitleProps) {
+  return (
+    <div
+      className={`leading-6 lg:leading-9 ${variant === 'dark' ? 'text-accent' : 'text-light'}`}
+      role="heading"
+      aria-label={headline}
+    >
+      <span className="italic font-light text-header-sm">{preheadline}</span>
+      <h2 className="font-bold uppercase text-header-md">{headline}</h2>
+    </div>
+  );
+}
