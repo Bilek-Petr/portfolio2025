45f3d3caa181ff6344245c65cf794e81d6fc7d90
Petr Bílek <p.bilek29@gmail.com>
2025-10-06 10:54:03 +0200

    [add] Create About component for displaying personal information and related sections

---
+++ src/components/sections/about/About.tsx
@@ -0,0 +1,67 @@
+'use client';
+import React from 'react';
+import AboutProfile from './AboutProfile';
+import aboutData from '@/data/about.json';
+import SectionTitle from '@/components/shared/sectionTitle/SectionTitle';
+import DownloadCTA from '@/components/ui/button/downloadCTA/DownloadCTA';
+import ContactList from '@/components/sections/about/ContactList';
+import BioItem from '@/components/sections/about/BioItem';
+import QuoteBlock from '@/components/sections/about/QuoteBlock';
+import { AboutData } from '@/types';
+
+const typedAboutData = aboutData as AboutData;
+
+export default function About() {
+  const {
+    preheadline,
+    headline,
+    name,
+    tagline,
+    image,
+    imageAlt,
+    cv,
+    contact,
+    bio,
+    quote,
+  } = typedAboutData;
+
+  return (
+    <>
+      <section id="about" className="py-8">
+        <SectionTitle preheadline={preheadline} headline={headline} />
+
+        <div className="flex flex-col items-center gap-10 pt-10 lg:gap-28 lg:pt-20 2xl:flex-row">
+          <div
+            aria-label="Profile details"
+            className="flex flex-col items-center"
+          >
+            <AboutProfile
+              name={name}
+              tagline={tagline}
+              image={image}
+              imageAlt={imageAlt}
+            />
+            <DownloadCTA cvPath={cv.link} label={cv.label} />
+          </div>
+
+          <div aria-label="More about me" className="flex-1">
+            <ContactList
+              contacts={contact}
+              className="grid grid-cols-1 gap-2 xs:grid-cols-2 md:gap-5"
+            />
+
+            <hr className="my-8 border-t border-accent/20" />
+
+            <div className="flex flex-col gap-6 xs:flex-row">
+              {bio.map((bioItem, index) => (
+                <BioItem key={index} bioInfo={bioItem} />
+              ))}
+            </div>
+
+            <QuoteBlock quote={quote} />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
